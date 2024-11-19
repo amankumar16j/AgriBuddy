81df69dac119cc1f61076bfd63f9a18121b83da1
Aman Kumar <111699887+amankumar16j@users.noreply.github.com>
2024-11-19 11:48:57 +0530

    Update news.py

--- mainapp/pages/news.py
+++ mainapp/pages/news.py
@@ -6,6 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 from io import BytesIO
+from streamlit_lottie import st_lottie
 
 
 st.set_page_config(layout="wide")
@@ -15,6 +16,26 @@ def load_image_from_url(url):
     response = requests.get(url)
     img = Image.open(BytesIO(response.content))
     return img
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json() 
+  
+col3=st.columns([1,4])
+with col3[0]:
+      link="https://lottie.host/e38e30a2-e829-4928-b271-e4b3f3569fb3/BK67yUyrvM.json"
+      l=load_lottieurl(link)
+      st.lottie(l,height=130,width=300)
+with col3[1]:
+    st.markdown("""
+                <h1 style='font-size: 60px;'>
+                    NEWS
+                </h1>
+                <hr style='border: 2px solid white;'>
+                
+                """, unsafe_allow_html=True)
 
 
 URL = "https://www.livemint.com/industry/agriculture"
@@ -41,55 +62,3 @@ for news,img in zip(cont,img_tags):
       
       with cols[1]:
         st.markdown(f'<a href="{full_link}" style="font-size:40px; text-decoration:none;">{text}</a>', unsafe_allow_html=True)
-        # st.header(f"{text}")
-        # st.write(f"{full_link}\n")
-
-
-# def fetch_news():
-#     conn = http.client.HTTPSConnection('api.thenewsapi.com')
-
-#     # Update the search term to focus on farming and agriculture
-#     params = urllib.parse.urlencode({
-#         'api_token': 'xhfC0dlBlxEl5P2r8Oi24ogQNFxgPY3EXGJMnM3n',  # Replace with your API token
-#         'search': 'farming OR agriculture OR farmer OR crop OR irrigation OR rural OR India OR "Indian farmers" OR "Indian agriculture" OR "government schemes"',  # Search terms for farming and agriculture
-#         'language': 'en',  # Filter to get English news
-#         'limit': 50,  # Limit the number of articles
-#     })
-
-#     conn.request('GET', '/v1/news/all?{}'.format(params))
-#     res = conn.getresponse()
-#     data = res.read()
-#     conn.close()
-
-#     # Decode and parse the response
-#     news_data = json.loads(data.decode('utf-8'))
-#     return news_data
-
-# # Display the news articles in Streamlit
-# def display_news(news_data):
-#     st.title("Latest Farmer and Agriculture News")
-    
-#     # Check if news data contains articles
-#     if 'data' in news_data:
-#         articles = news_data['data']
-        
-#         # Loop through articles and display in Streamlit
-#         for article in articles:
-#             st.subheader(article['title'])
-#             st.write(article['description'])
-#             st.markdown(f"[Read more]({article['url']})")
-#             st.markdown("---")  # Separator between articles
-#     else:
-#         st.write("No news articles available.")
-
-# # Main Streamlit app function
-# def main():
-#     # Fetch news data
-#     news_data = fetch_news()
-    
-#     # Display news in Streamlit
-#     stx.scrollableTextbox(
-#     display_news(news_data))
-
-# if __name__ == "__main__":
-#     main()
